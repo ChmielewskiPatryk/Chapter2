c2bf24c2a985f358a08103a05e9380f81b0e08d6
Jasiu Imac <jan@kaletka.com.pl>
2022-10-11 14:54:54 +0200

    Done

--- OOP/OOP-Day2/src/main/java/org/example/AdvancedCalculator.java
+++ OOP/OOP-Day2/src/main/java/org/example/AdvancedCalculator.java
@@ -9,6 +9,12 @@
 //statyczną metodę computeCircleArea(r), która będzie zwracała pole koła. Ta metoda nie będzie dopisywać obliczeń do tablicy (napisz w komentarzu, dlaczego nie może tego robić),
 //statyczną tablicę, która będzie przechowywała historię operacji wykonanych na wszystkich kalkulatorach,
 //statyczną metodę printGlobalOperations(), która będzie wyświetlała wszystkie operacje ze wszystkich obiektów klasy AdvancedCalculator.
+
+// ZAD 2
+//Do klasy AdvancedCalculator dopisz:
+//
+//przeciążoną metodę printGlobalOperations(int length), która wyświetli określoną liczbę ostatnich operacji,
+//przeciążoną metodę printGlobalOperations(String length), która wyświetli określoną liczbę ostatnich operacji.
 package org.example;
 
 import java.util.Arrays;
@@ -49,6 +55,37 @@ public class AdvancedCalculator extends Calculator {
         }
     }
 
-    ;
+    public static void printGlobalOperations(int length) {
+        if (globalArr.length > length) {
+            for (int i = length; i > 0; i--) {
+                System.out.println(globalArr[globalArr.length - 1 - i]);
+            }
+        } else {
+            System.out.println("Array is not long enough");
+            for (String item : globalArr) {
+                System.out.println(item);
+            }
+        }
+    }
+
+    public static void printGlobalOperations(String length) {
+        try {
+            int len = Integer.parseInt(length);
+            if (globalArr.length > len) {
+                for (int i = len; i > 0; i--) {
+                    System.out.println(globalArr[globalArr.length - 1 - i]);
+                }
+            } else {
+                System.out.println("Array is not long enough");
+                for (String item : globalArr) {
+                    System.out.println(item);
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Wrong argument");
+        }
+
+    }
+
 
 }
